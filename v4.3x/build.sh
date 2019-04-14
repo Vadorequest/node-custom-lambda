27f0d809ed8d809ab328dcd1d60f6a13fd76d355
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-04-14 12:32:08 +0200

    Cleanup

--- v4.3x/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export NODE_VERSION=4.3.2
-
-docker build --build-arg NODE_VERSION -t node-provided-lambda-v4.3 .
-docker run --rm node-provided-lambda-v4.3 cat /tmp/node-v${NODE_VERSION}.zip > ./layer.zip
